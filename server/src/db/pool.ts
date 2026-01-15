fc9a104c0bf17f61788a03df9e76b3e5ce828e76
Abdul Karim <karim.develops@gmail.com>
2026-01-15 15:06:31 +0500

    Setup db

---
+++ server/src/db/pool.ts
@@ -0,0 +1,26 @@
+import "dotenv/config"
+import { Pool } from "pg"
+
+function poolConfig() {
+    return new Pool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        database: process.env.DB
+    })
+}
+
+const pool = poolConfig();
+
+async function verifyConnection(): Promise<void> {
+    try {
+        const client = await pool.connect();
+        console.log("Connected to db successfully.");
+        client.release();
+    } catch (err) {
+        console.log(`Error occurred connecting to db: ${err}`)
+    }
+}
+
+verifyConnection();
+
+export default pool;
