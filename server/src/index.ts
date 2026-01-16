07bf7536479d381e0cdbef230b0e2a1d180ebf3f
Abdul Karim <karim.develops@gmail.com>
2026-01-17 01:30:21 +0500

    Set directory to serve static image files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,17 @@
 import "dotenv/config";
+import path from "node:path";
 import express from "express";
 import adminRouter from "./routes/admin.ts";
 import menuRouter from "./routes/menu.ts";
 
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = import.meta.dirname;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use("/api/uploads", express.static(path.join(__dirname, "..", "uploads")));
+console.log(path.join(__dirname, "uploads"));
 app.use("/api/admin", adminRouter);
 app.use("/api/menu", menuRouter);
 
