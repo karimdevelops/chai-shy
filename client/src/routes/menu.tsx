f90c5acb28a3b69e38443608d47b32e0d8bca514
Abdul Karim <karim.develops@gmail.com>
2026-01-17 15:08:37 +0500

    Redirect user on parent route

--- client/src/routes/menu.tsx
+++ client/src/routes/menu.tsx
@@ -1,6 +1,15 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/menu")({
+  beforeLoad: ({ location }) => {
+    if (location.pathname === "/menu") {
+      throw redirect({
+        to: "/menu/$cat",
+        params: { cat: "drinks-1" },
+        replace: true,
+      });
+    }
+  },
   component: MenuComponent,
 });
 
