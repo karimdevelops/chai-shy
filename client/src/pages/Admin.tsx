90bc29b70d992dd688dbcbcd191d539f037ee320
Abdul Karim <karim.develops@gmail.com>
2026-01-21 12:42:05 +0500

    Add graphfor admin data

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { Link } from "@tanstack/react-router";
-import "../styles/Admin.css";
+import { BarChart } from "@mui/x-charts";
 import AddForm from "../components/AddForm";
+import "../styles/Admin.css";
 
 export default function Admin() {
   const [isForm, setIsForm] = useState(false);
@@ -12,31 +13,104 @@ export default function Admin() {
   }
 
   return (
-    <div className="mheight-70 flex flex-center flex-gap-50">
-      <button className="button-work flex flex-column flex-center flex-gap-20">
-        <img src="./icons/approve.svg" alt="Verify" />
-        <div>Aprove Staff</div>
-      </button>
-      <Link
-        className="button-work flex flex-column flex-center flex-gap-20"
-        to="/support-chat"
-      >
-        <img src="./icons/support.svg" alt="Support" />
-        <div>Support Chat</div>
-      </Link>
-      <button className="button-work flex flex-column flex-center flex-gap-20">
-        <img src="./icons/approve.svg" alt="Verify" />
-        <div>Order History</div>
-      </button>
-      <button
-        className="button-work flex flex-column flex-center flex-gap-20"
-        onClick={toggleForm}
-      >
-        <img src="./icons/add.svg" alt="Add food" />
-        <div>Add Food</div>
-      </button>
+    <div className="grid grid-2fr-1fr">
+      <div>
+        <BarChart
+          sx={{
+            "& .MuiChartsAxis-tickContainer .MuiChartsAxis-tickLabel": {
+              fill: "white",
+            },
+            "& .MuiChartsAxis-line": {
+              strokeWidth: "2px",
+              stroke: "white",
+            },
+          }}
+          series={[
+            {
+              data: [2, 5, 3, 4, 4, 2, 5, 3, 4, 4],
+            },
+          ]}
+          xAxis={[
+            {
+              id: "barCategories",
+              data: [
+                "bar A",
+                "bar B",
+                "bar C",
+                "bar D",
+                "bar E",
+                "bar F",
+                "bar G",
+                "bar H",
+                "bar I",
+                "bar J",
+              ],
+            },
+          ]}
+          height={500}
+          width={500}
+        />
+      </div>
+      <div className="flex flex-column flex-gap-50">
+        <div className="grid grid-1fr-1fr grid-gap-20">
+          <div>
+            <h3>Total Sales</h3>
+            <h4>$55.9</h4>
+          </div>
+          <div>
+            <h3>Total Sales</h3>
+            <h4>$55.9</h4>
+          </div>
+          <div>
+            <h3>Total Sales</h3>
+            <h4>$55.9</h4>
+          </div>
+          <div>
+            <h3>Total Sales</h3>
+            <h4>$55.9</h4>
+          </div>
+        </div>
+        <div className="grid grid-1fr-1fr-1fr grid-gap-20">
+          <button className="button-work flex flex-column flex-center flex-gap-20">
+            <img src="./icons/approve.svg" alt="Verify" />
+            <div>Aprove Staff</div>
+          </button>
+          <Link
+            className="button-work flex flex-column flex-center flex-gap-20"
+            to="/support-chat"
+          >
+            <img src="./icons/support.svg" alt="Support" />
+            <div>Support Chat</div>
+          </Link>
+          <button className="button-work flex flex-column flex-center flex-gap-20">
+            <img src="./icons/approve.svg" alt="Verify" />
+            <div>Order History</div>
+          </button>
+          <button
+            className="button-work flex flex-column flex-center flex-gap-20"
+            onClick={toggleForm}
+          >
+            <img src="./icons/add.svg" alt="Add food" />
+            <div>Add Food</div>
+          </button>
+          <button
+            className="button-work flex flex-column flex-center flex-gap-20"
+            onClick={toggleForm}
+          >
+            <img src="./icons/add.svg" alt="Add food" />
+            <div>Add Food</div>
+          </button>
+          <button
+            className="button-work flex flex-column flex-center flex-gap-20"
+            onClick={toggleForm}
+          >
+            <img src="./icons/add.svg" alt="Add food" />
+            <div>Add Food</div>
+          </button>
 
-      {isForm ? <AddForm /> : null}
+          {isForm ? <AddForm /> : null}
+        </div>
+      </div>
     </div>
   );
 }
