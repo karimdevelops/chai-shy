eceeb4266d41039f833935e56130f9fafa616ec1
Abdul Karim <karim.develops@gmail.com>
2026-05-01 18:29:17 +0500

    style: index page layout for small devices

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,31 +3,35 @@ import { Link } from "@tanstack/react-router";
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center items-center gap-12 p-8">
-        <div className="flex flex-col max-w-lg gap-10">
+      <div className="flex flex-col items-center justify-center gap-12 py-8 md:flex-row md:px-8">
+        <div className="flex max-w-lg flex-col gap-10">
           <div>
-            <h5 className="font-playfair text-base font-bold">
+            <h5 className="font-playfair text-sm font-bold md:text-base">
               Chase The Classic Flavour
             </h5>
-            <h1 className="font-bold">
+            <h1 className="font-playfair text-primary text-5xl leading-normal font-bold md:text-6xl">
               The Key to <br /> Fine Chuski Exp.
             </h1>
           </div>
-          <p className="text-info text-lg">
+          <p className="text-info text-base">
             <strong>Chai Shy.</strong> One of the finest places in some place
             where you can get the desi tea for less than $5, even though the
             taste is worth a million times more! <i>slurps...</i>
           </p>
           <Link
             to="/menu/"
-            className="font-playfair text-lg text-app-background bg-primary max-w-fit px-6 py-4"
+            className="font-playfair text-app-background bg-primary max-w-fit px-6 py-4 text-lg"
           >
             Explore Menu
           </Link>
         </div>
-        <div className="flex flex-col hero">
+        <div className="hero hidden md:flex md:flex-col">
           <figure className="flex">
-            <img className="h-125 w-auto" src="./hero.avif" alt="Tea cups" />
+            <img
+              className="max-h-125 w-auto"
+              src="./hero.avif"
+              alt="Tea cups"
+            />
             <figcaption>
               <a
                 className="link-credits"
