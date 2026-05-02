ce57bd46e9c762ca0a45c8283f739605864c3161
Abdul Karim <karim.develops@gmail.com>
2026-05-02 22:47:57 +0500

    feat: add password to db

--- api/src/db/pool.ts
+++ api/src/db/pool.ts
@@ -10,6 +10,7 @@ function poolConfig() {
     return new Pool({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
       database: process.env.DB,
     });
   }
